ceec9d82640d24825c18563b342714462accf7e4
Chenwenfa <1151293565@qq.com>
2017-10-15 15:06:53 +0800

    rfgsdfgfg

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetupPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
@@ -110,7 +111,9 @@ public class HomeActivity extends AppCompatActivity {
                     mInPswdDialog.dismiss();
                     //进入防盗主界面
                     mInPswdDialog.dismiss();
-                    Toast.makeText(HomeActivity.this,"可以进入手机防盗模块",Toast.LENGTH_LONG).show();
+                    startActivity(LostFindActivity.class);
+//                    Toast.makeText(HomeActivity.this,"可以进入手机防盗模块",Toast.LENGTH_LONG).show();
+
                 }else{
                     //对话框消失，弹出Toast
                     mInPswdDialog.dismiss();
