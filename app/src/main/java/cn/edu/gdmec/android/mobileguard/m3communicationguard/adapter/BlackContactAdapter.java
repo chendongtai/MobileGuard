7bb69e6e5d5bec25d43b02a6c808b95438e6f1ef
chendongtai <850394542@qq.com>
2017-11-11 13:32:53 +0800

    添加黑名单号码类型

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m3communicationguard/adapter/BlackContactAdapter.java
@@ -6,6 +6,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 import java.util.List;
 import cn.edu.gdmec.android.mobileguard.R;
 import cn.edu.gdmec.android.mobileguard.m3communicationguard.db.dao.BlackNumberDao;
@@ -19,6 +22,7 @@ public class BlackContactAdapter extends BaseAdapter {
     class ViewHolder{
         TextView mNameTV;
         TextView mModeTV;
+        TextView mTypeTV;
         View mContactImgv;
         View mDeleteView;
     }
@@ -55,6 +59,7 @@ public class BlackContactAdapter extends BaseAdapter {
             holder = new ViewHolder();
             holder.mNameTV = (TextView) view.findViewById(R.id.tv_black_name);
             holder.mModeTV = (TextView) view.findViewById(R.id.tv_black_mode);
+            holder.mTypeTV = (TextView) view.findViewById(R.id.tv_black_type);
             holder.mContactImgv = view.findViewById(R.id.view_black_icon);
             holder.mDeleteView = view.findViewById(R.id.view_black_delete);
             view.setTag(holder);
@@ -63,8 +68,10 @@ public class BlackContactAdapter extends BaseAdapter {
         }
         holder.mNameTV.setText(contactInfos.get(position).contactName+"("+contactInfos.get(position).phoneNumber+")");
         holder.mModeTV.setText(contactInfos.get(position).getModeString(contactInfos.get(position).mode));
+        holder.mTypeTV.setText(contactInfos.get(position).type);
         holder.mNameTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
         holder.mModeTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
+        holder.mTypeTV.setTextColor(context.getResources().getColor(R.color.bright_purple));
         holder.mContactImgv.setBackgroundResource(R.drawable.brightpurple_contact_icon);
         holder.mDeleteView.setOnClickListener(new View.OnClickListener() {
             @Override
